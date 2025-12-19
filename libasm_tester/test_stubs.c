ff1904ecaf89388dc507652633de76aef6c870de
rabeloguedes <rabeloguedes@proton.me>
2025-12-19 13:56:27 +0000

    feat: Implements ft_list_remove_if tester.

--- libasm_tester/test_stubs.c
+++ libasm_tester/test_stubs.c
@@ -1,11 +1,3 @@
 #include "libtest.h"
 
 // Stub functions for tests not yet implemented
-
-void test_ft_list_remove_if(t_test_stats *stats)
-{
-	(void)stats;
-	print_header("Testing ft_list_remove_if [BONUS]");
-	if (g_verbose_mode)
-		printf("%s  Tests not yet implemented.%s\n\n", COLOR_YELLOW, COLOR_RESET);
-}
